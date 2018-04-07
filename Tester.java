4d95ff72496a6d37763368a9dd994cc707022a21
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-07 15:29:04 +0000

    Add input data

---
+++ Tester.java
@@ -0,0 +1,11 @@
+//Author: Jacob Liddicoat
+//Date Created: Apr 7,2018
+import java.io.*;
+import java.util.*;
+public class Tester
+{
+ public static void main(String args[])
+ {
+  system.out.println("Please enter your name. Jacob Liddicoat");
+  {
+   
